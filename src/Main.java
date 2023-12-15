f0a03e9145001896ba3514aedac2bad8068f9637
nour-atef04 <nourmohamedatef04@gmail.com>
2023-12-15 19:32:13 +0200

    override clown's setX

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,6 @@ import eg.edu.alexu.csd.oop.game.GameEngine.*;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class Main {
     public static void main(String[] args) {
@@ -21,33 +19,6 @@ public class Main {
         menu.add(pauseMenuItem);
         menu.add(resumeMenuItem);
         menuBar.add(menu);
-
-
         GameController gc =  GameEngine.start("Circus of Plates", new Circus(900, 700), menuBar, Color.BLACK);
-
-        pauseMenuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Handle the "Pause" menu item action
-                gc.pause();
-            }
-        });
-
-        resumeMenuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Handle the "Resume" menu item action
-                gc.resume();
-            }
-        });
-
-        newMenuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Handle the "Pause" menu item action
-                gc.changeWorld(new Circus(900, 700));
-            }
-        });
-
         }
     }
